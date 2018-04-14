2b00696c376b03c1f49cfcd72da25288ce4e6b80
T.J. Hooker <35381752+tyrelhooker@users.noreply.github.com>
2018-04-13 23:21:20 -0400

    change OW call

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -134,7 +134,7 @@ $(".jumbotron").on("click", function(){
         // This is our API key and variables for the queryURL
         var APIKey = "7d2ff8f5647ce6dbd5231ca3f107d20b";
         // The URL to query the database
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + localLocation.lat + "&lon=" + localLocation.lon + "&appid=" + APIKey;
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + localLocation.latitude + "&lon=" + localLocation.longitude + "&appid=" + APIKey;
         // AJAX call to the OpenWeatherMap API
         $.ajax({
             url: queryURL,
